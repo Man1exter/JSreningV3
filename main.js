66eef1ec6d82581f4fae5da9fa61c5fd7809b6d9
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-25 11:12:45 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+const changeColor = () => {
+if(e.keyCode === 38){
+    
+}
+}
+document.body.style.backgroundColor = `rgb ${red}, ${green}, ${blue}`;
+
+
+document.body.addEventListener("keydown", changeColor)
