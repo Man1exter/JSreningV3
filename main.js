019ca7fc2d028c058ebaab0b1e5fd4e95f7d4938
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-25 11:25:04 +0200

    arrow up and down effect on keycode

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ let blue = 100;
 
 const changeColor = (e) => {
 if(e.keyCode === 38 && red < 255){
-red += 1;
-green += 1;
-blue += 1;
-document.body.style.backgroundColor = `rgb (${red}, ${green}, ${blue})`;
+red += 2;
+green += 2;
+blue += 2;
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
- else if(e.keyCode === 40 && red > 255){
+ else if(e.keyCode === 40 && red >= 0){
 red -= 1; //red--;
 green -= 1; //green--
 blue -= 1; //blue--;
@@ -20,4 +20,4 @@ document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 
 
-document.body.addEventListener("keyCode", changeColor)
+window.addEventListener("keydown", changeColor)
