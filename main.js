e7424c8ba6784d41f72c8ee02f0a32de108b265d
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-25 11:19:29 +0200

    arrow up arrow down effe

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ let red = 100;
 let green = 100;
 let blue = 100;
 
-const changeColor = () => {
-if(e.keyCode === 38){
-    
+const changeColor = (e) => {
+if(e.keyCode === 38 && red < 255){
+red += 1;
+green += 1;
+blue += 1;
+document.body.style.backgroundColor = `rgb (${red}, ${green}, ${blue})`;
+}
+
+ else if(e.keyCode === 40 && red > 255){
+red -= 1; //red--;
+green -= 1; //green--
+blue -= 1; //blue--;
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 }
-document.body.style.backgroundColor = `rgb ${red}, ${green}, ${blue}`;
 
 
-document.body.addEventListener("keydown", changeColor)
+
+document.body.addEventListener("keyCode", changeColor)
