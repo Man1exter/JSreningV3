db9c33a9d995bbad6606560aab88205643d49a4e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-25 11:48:22 +0200

    finish arrow up down with switch

--- main.js
+++ main.js
@@ -7,22 +7,45 @@ let red = 100;
 let green = 100;
 let blue = 100;
 
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 const changeColor = (e) => {
-if(e.keyCode === 38 && red < 255){
-red += 2;
-green += 2;
-blue += 2;
-document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+// document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+// const changeColor = (e) => {
+// if(e.keyCode === 38 && red < 255){
+// red += 2;
+// green += 2;
+// blue += 2;
+// document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// }
+
+//  else if(e.keyCode === 40 && red >= 0){
+// red -= 1; //red--;
+// green -= 1; //green--
+// blue -= 1; //blue--;
+// document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// }
+// }
+
+// switch switch switch -----------------------------
+//----------------------- switch switch switch ---
+
+switch(e.keyCode){
+    case 38:
+        document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
+        break;
+
+    case 40:
+        document.body.style.backgroundColor = `rgb(${red > 0 ? red-- : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+        break;
+
+        //color wiekszy/mniejszy (od) ? oddejmij/dodaj : do.
+        //color wiekszy/mniejszy (od) ? oddejmij/dodaj : do.
+        //color wiekszy/mniejszy (od) ? oddejmij/dodaj : do.
+
 }
 
- else if(e.keyCode === 40 && red >= 0){
-red -= 1; //red--;
-green -= 1; //green--
-blue -= 1; //blue--;
-document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
 }
-}
-
-
-
 window.addEventListener("keydown", changeColor)
